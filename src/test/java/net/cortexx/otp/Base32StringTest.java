61e208d96dbb7a89e19dea46730b2e15abfedc4c
CK <cklee7755@gmail.com>
2013-11-29 18:08:57 +0800

    random secret key

--- src/test/java/net/cortexx/otp/Base32StringTest.java
+++ src/test/java/net/cortexx/otp/Base32StringTest.java
@@ -1,8 +1,12 @@
 package net.cortexx.otp;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import java.security.SecureRandom;
+
 import net.cortexx.otp.Base32String.DecodingException;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 
 public class Base32StringTest {
@@ -10,14 +14,23 @@ public class Base32StringTest {
 
 	@Test
 	public void testBase32String() throws DecodingException {
-		// Need at least 1 byte, 8 characters: twenty-six letters A-Z and six digits 2-7
-		String secret = "ABCD7777";
+		// twenty-six letters A-Z and six digits 2-7
+		String acceptedString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+		char[] chars = acceptedString.toCharArray();
+		// Need multiple of 1 byte/8 characters:
+		int count = 16;
+		// Cheaper but not true random
+		// String secret = RandomStringUtils.random(16, acceptedString);
+		String secret = RandomStringUtils.random(count, 0, chars.length, false, false, chars, new SecureRandom());
+		assertEquals(secret.length(), count);
 		byte[] afterDecode = Base32String.decode(secret);
 		System.out.println(afterDecode);
 		String afterEncode = Base32String.encode(afterDecode);
 		System.out.println(afterEncode);
-		assertTrue(secret.equals(afterEncode));
+		assertEquals(secret, afterEncode);
 		
 	}
+	
+	
 
 }
